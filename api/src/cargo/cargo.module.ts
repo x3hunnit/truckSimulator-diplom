f27b4e5d279ce24f414a4a20ce44c05cd9b376cd
Dmitry Uchkin <125159427+wakeuphaku@users.noreply.github.com>
2025-03-09 17:06:37 +0300

    asd

--- api/src/cargo/cargo.module.ts
+++ api/src/cargo/cargo.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CargoService } from './cargo.service';
 import { CargoController } from './cargo.controller';
-import  { PrismaModule } from '../../prisma/prisma.module'
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   providers: [CargoService],
-  controllers: [CargoController]
+  controllers: [CargoController],
 })
 export class CargoModule {}
